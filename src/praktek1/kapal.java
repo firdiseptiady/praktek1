24b9b350ad53ddb8a448289edf7f9473478d8dda
firdiseptiady <firdis998@gmail.com>
2018-03-26 20:17:13 +0800

    membuat atribut dan behavior

--- src/praktek1/kapal.java
+++ src/praktek1/kapal.java
@@ -10,6 +10,18 @@ package praktek1;
  * @author TWINCOM
  */
 public class kapal {
+    String Jenis_kapal;
+    String Bentuk_kapal;
+    String Warna_kapal;
+    String Tempat_duduk;
+    String Pelampung;
     
+    void cetakInfo(){
+        System.out.println("Jenis_kapal :"+Jenis_kapal);
+        System.out.println("Bentuk_kapal :"+Bentuk_kapal);
+        System.out.println("Warna_kapal :"+Warna_kapal);
+        System.out.println("Tempat_duduk :"+Tempat_duduk);
+        System.out.println("Pelampung :"+Pelampung);
+    }
     
 }
